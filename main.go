52e6689857ec36f57edfc649a0d6056b2a763907
Alfred <alfredsklotsu@gmail.com>
2023-03-21 12:49:52 +0000

    initial commit, all routes added

--- main.go
+++ main.go
@@ -2,14 +2,41 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	"gonic-models/database"
+	"gonic-models/routes"
+	"log"
 )
-var Router * gin.Engine
+
+func init() {
+	client := database.Connect()
+	if client == nil {
+		log.Println("There is an error with your mongo client")
+	}
+	if client != nil {
+
+		log.Println("Your mongo client is working as expected")
+	}
+
+}
+
 func main() {
-	r := gin.Default()
-	r.GET("/", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "Hello world!",
-		})
+	router := gin.Default()
+
+	router.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{"success": "You were successful in getting the Api 🇬🇭"})
+
 	})
-	r.Run()
+
+	router.POST("/new", routes.AddCountry())
+	router.GET("/all", routes.GetCountries())
+	router.GET("/all/sorted", routes.GetCountriesSorted())
+	router.GET("/all/:id", routes.GetCountry())
+	router.DELETE("/del/:id", routes.DeleteCountry())
+	router.PUT("/repl/:id", routes.ReplaceCountry())
+	router.PATCH("/upd/:id", routes.UpdateCountry())
+
+	if err := router.Run(":8083"); err != nil {
+		log.Println("Could not start router at said port", err)
+		log.Fatal()
+	}
 }
